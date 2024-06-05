1f7cb2e63de12814abebd29be9d983ea545edc06
monmun018 <quymao08101999@gmail.com>
2024-06-05 15:55:37 +0900

    追加：showBbs 修正: CommentForm field

--- src/main/java/com/example/controller/ShowBbsController.java
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -1,9 +1,12 @@
 package com.example.controller;
 
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
+import com.example.form.CommentForm;
 import com.example.repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,9 +22,9 @@ public class ShowBbsController {
     ArticleRepository articleRepository;
 
     @GetMapping("")
-    public String index(){
+    public String index(ArticleForm articleForm, CommentForm commentForm, Model model){
         List<Article> articleList = articleRepository.findAll();
-        System.out.println(articleList.size());
+        model.addAttribute("articleList",articleList);
         return "article";
     }
 }
